063c24b27aef91206641b998953090a2bc4a664d
石川達也 <tatsuya7610@icloud.com>
2021-01-17 01:43:14 +0900

    11th commit

--- lib/helper/firebase_errors.dart
+++ lib/helper/firebase_errors.dart
@@ -1,25 +1,54 @@
 String getErrorString(String code){
+  //PlatformExceptionでフリーズ。
+  //PlatformExceptionをキャッチできないように見えるが実際はキャッチできている。
+  //対応策、AndroidStudio > 実行 > ブレークポイント表示 > Break on exceptionsを使用可能にチェック。
+  //https://python5.com/q/rkqqyfobを参照。その他ページも該当操作推進。
   switch (code) {
-    case 'ERROR_WEAK_PASSWORD':
+    case 'weak-password':
       return 'パスワードの安全性が低いです';
-    case 'ERROR_INVALID_EMAIL':
+    case 'invalid-email':
       return 'メールアドレスが無効です';
-    case 'ERROR_EMAIL_ALREADY_IN_USE':
+    case 'email-already-in-use':
       return 'すでに別のアカウントで使用されています';
-    case 'ERROR_INVALID_CREDENTIAL':
+    case 'invalid-credential':
       return 'メールアドレスが無効です';
-    case 'ERROR_WRONG_PASSWORD':
+    case 'wrong-password':
       return 'パスワードが間違えています';
-    case 'ERROR_USER_NOT_FOUND':
+    case 'user-not-found':
       return 'このメールアドレスは登録されていません';
-    case 'ERROR_USER_DISABLED':
+    case 'user-disabled':
       return 'このユーザーは無効になっています';
-    case 'ERROR_TOO_MANY_REQUESTS':
+    case 'too-many-requests':
       return 'リクエストが多いです。後でもう一度お試しください';
-    case 'ERROR_OPERATION_NOT_ALLOWED':
+    case 'operation-not-allowed':
       return '操作は許可されていません';
 
     default:
       return '予測不可能なエラーが発生しました';
+
+  //下記、firebase公式。ただし、返されるエラーコードは小文字。バージョンアップから？
+
+  //   case 'ERROR_WEAK_PASSWORD':
+  //     return 'パスワードの安全性が低いです';
+  //   case 'ERROR_INVALID_EMAIL':
+  //     return 'メールアドレスが無効です';
+  //   case 'ERROR_EMAIL_ALREADY_IN_USE':
+  //     return 'すでに別のアカウントで使用されています';
+  //   case 'ERROR_INVALID_CREDENTIAL':
+  //     return 'メールアドレスが無効です';
+  //   case 'ERROR_WRONG_PASSWORD':
+  //     return 'パスワードが間違えています';
+  //   case 'ERROR_USER_NOT_FOUND':
+  //     return 'このメールアドレスは登録されていません';
+  //   case 'ERROR_USER_DISABLED':
+  //     return 'このユーザーは無効になっています';
+  //   case 'ERROR_TOO_MANY_REQUESTS':
+  //     return 'リクエストが多いです。後でもう一度お試しください';
+  //   case 'ERROR_OPERATION_NOT_ALLOWED':
+  //     return '操作は許可されていません';
+  //
+  //   default:
+  //     return '予測不可能なエラーが発生しました';
   }
+
 }
